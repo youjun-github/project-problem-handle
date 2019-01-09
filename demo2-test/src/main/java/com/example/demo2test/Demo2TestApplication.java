c9ec712bb2664c2e3a4a7fad29cf14c36f381986
youjun <youjun@sao.so>
2019-01-09 11:37:40 +0800

    init.

---
+++ demo2-test/src/main/java/com/example/demo2test/Demo2TestApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo2test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo2TestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo2TestApplication.class, args);
+    }
+
+}
+
---
+++ demo2-test/src/main/java/com/example/demo2test/Test.java
@@ -0,0 +1,11 @@
+package com.example.demo2test;
+
+/**
+ * @author youjun
+ * @create 2019-01-09 11:14
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("111");
+    }
+}
